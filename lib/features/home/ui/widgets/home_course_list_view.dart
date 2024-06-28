8a4fde5f01f10b50662a2e3d19bfaa07a08aef37
AMIRAMOHMED <amiramohmaed887@gmail.com>
2024-06-28 15:51:07 +0300

    finish ui of home screeen

--- lib/features/home/ui/widgets/home_course_list_view.dart
+++ lib/features/home/ui/widgets/home_course_list_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tech_savvy/core/extension/context_extension.dart';
+import 'package:tech_savvy/core/language/lang_key.dart';
+import 'package:tech_savvy/core/style/fonts/app_styles.dart';
+import 'package:tech_savvy/core/widgets/app_text.dart';
 import 'package:tech_savvy/features/home/ui/widgets/home_container_course.dart';
 
 class HomeCourseListView extends StatelessWidget {
@@ -7,16 +11,28 @@ class HomeCourseListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 165.h,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: 5,
-        
-        itemBuilder: (BuildContext context, int index) {
-          return const HomeContainerCourse();
-        },
-      ),
+    return Stack(
+      children: [
+        SizedBox(
+          height: 165.h,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: 5,
+            itemBuilder: (BuildContext context, int index) {
+              return const HomeContainerCourse();
+            },
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.sp),
+          child: TextApp(
+            text: context.translate(LangKeys.whatDoYouWantToLearnToday),
+            style: AppStyles.font500primary18(context).copyWith(
+              color: context.color.mainFontColor2,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
