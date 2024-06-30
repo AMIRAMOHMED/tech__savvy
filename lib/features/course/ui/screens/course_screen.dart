3963324b369e56a0dd788822ebe94bca11887b34
AMIRAMOHMED <amiramohmaed887@gmail.com>
2024-06-30 12:06:34 +0300

    added some words to locallaztion

--- lib/features/course/ui/screens/course_screen.dart
+++ lib/features/course/ui/screens/course_screen.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:tech_savvy/features/course/ui/widgets/course_screen_app_bar.dart';
 
 class CourseScreen extends StatelessWidget {
   const CourseScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Container(child: Text('Course Screen'),));
+    return const Scaffold(
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            children: [
+              CourseScreenAppBar(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/features/course/ui/widgets/course_screen_app_bar.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/widgets.dart';
+
+class CourseScreenAppBar extends StatelessWidget {
+  const CourseScreenAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            
+            
+          ],
+        ),
+      ],
+    );
+  }
+}
